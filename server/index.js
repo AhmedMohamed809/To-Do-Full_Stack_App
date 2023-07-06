ff546c34c88936d250152cb44ac78390e3251b7a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-06 06:27:17 +0100

    connect database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-
+const pool = require("./db")
 //middleware 
 app.use(cors());
 app.use(express.json);
 
-
+// Routes 
 app.listen(500,()=>{
     console.log("server has started >> port 5000 ");
 })
