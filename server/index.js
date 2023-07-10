1a0408f3c0f40bedf31eadb5533c8adb42bb2dbf
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-10 13:06:19 +0100

    fix database connection port

--- server/index.js
+++ server/index.js
@@ -4,9 +4,30 @@ const cors = require("cors")
 const pool = require("./db")
 //middleware 
 app.use(cors());
-app.use(express.json);
+app.use(express.json()); // req.body
 
-// Routes 
-app.listen(500,()=>{
+// Routes//
+
+//create todo 
+app.post("/todos",async(req,res)=>{
+try {
+    const {description}=req.body;
+    const newTodo= await pool.query(
+    "INSERT INTO todo (description) VALUES($1)",
+    [description]
+    );
+    res.json(newTodo);
+} catch (err) {
+    console.error(err.message);
+}
+})
+// get all todo 
+
+//get a todo
+
+//update a todo 
+
+//delete a todo
+app.listen(5000,()=>{
     console.log("server has started >> port 5000 ");
 })
