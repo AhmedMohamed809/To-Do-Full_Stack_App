c86341ecba569aa06fb08065c8a0d4aa106a61ef
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-11 05:18:03 +0100

    get all todo and filter one todo

--- server/index.js
+++ server/index.js
@@ -13,18 +13,36 @@ app.post("/todos",async(req,res)=>{
 try {
     const {description}=req.body;
     const newTodo= await pool.query(
-    "INSERT INTO todo (description) VALUES($1)",
+    "INSERT INTO todo (description) VALUES($1) RETURNING *",
     [description]
     );
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
 } catch (err) {
     console.error(err.message);
 }
 })
 // get all todo 
+app.get("/todos",async(req,res)=>{
+    try {
+
+        const alltodo = await pool.query("SELECT * FROM todo");
+        res.json(alltodo.rows)
+        
+    } catch (err) {
+        console.error(err.message)
+    }
+} )
 
 //get a todo
-
+app.get("/todos/:id", async (req,res)=>{
+    try {
+        const todoID =Number(req.params.id);
+        const filterTodo= await pool.query("SELECT * FROM todo WHERE todo_id= $1",[todoID]);
+        res.json(filterTodo.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 //update a todo 
 
 //delete a todo
