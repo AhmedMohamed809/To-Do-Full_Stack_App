9744224c4881361a586f15428fb9559901c18d3b
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-12 05:57:06 +0100

    add fetch and POST request

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,25 @@
 import React ,{Fragment ,useState} from 'react'
 
 function InputTodo() {
-    const [description,setDescription] = useState("")
+    const [description,setDescription] = useState("");
+    const onSubmitForm = async(e) =>{
+        e.preventDefault();
+        try {
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(body)
+            });
+            console.log(response);
+         } catch (err) {
+            console.error(err.massage);
+        }
+    }
   return (
     <Fragment>
         <h1 className='text-center mt-5'>To do List</h1>
-        <form className='d-flex'>
+        <form className='d-flex' onSubmit={onSubmitForm}>
             <input type='text' className='form-control' 
             value={description} 
             onChange={e=>setDescription(e.target.value)}
