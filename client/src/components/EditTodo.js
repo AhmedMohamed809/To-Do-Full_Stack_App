7de6c25eb753068794c38da4a128a501f6a102e7
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-14 05:47:05 +0100

    change the color of  edit button

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -3,7 +3,7 @@ import React ,{Fragment} from 'react'
 function EditTodo() {
   return (
     <Fragment>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+<button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal">
   Edit
 </button>
 
@@ -14,16 +14,18 @@ function EditTodo() {
 
       
       <div class="modal-header">
-        <h4 class="modal-title">Modal Heading</h4>
+        <h4 class="modal-title">Edit Todo</h4>
         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
       </div>
 
       <div class="modal-body">
-        Modal body..
+        <input type='text' className='form-control'/>
       </div>
 
       
       <div class="modal-footer">
+      <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Edit</button>
+
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
       </div>
 
