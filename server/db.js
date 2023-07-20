54417ff1d64f4c68aac852170756615c53b96eed
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-20 22:48:00 +0100

    CREATE connectionString

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool
-
+const connectionString = process.env.DATABASE_URL;
 const pool =new Pool({
-    user : "postgres",
-    password:"0007",
-    host:"localhost",
-    port:5432,
-    database:"perntodo"
+    // user : "postgres",
+    // password:"0007",
+    // host:"localhost",
+    // port:5432,
+    // database:"perntodo"
+    connectionString
 })
 
 module.exports =pool;
