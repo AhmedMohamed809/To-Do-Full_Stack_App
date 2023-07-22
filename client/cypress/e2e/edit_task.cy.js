b6fbf305888b03798fc1652ff32d619c67f0370e
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-07-22 16:34:28 +0100

    create e2e test to edit task

---
+++ client/cypress/e2e/edit_task.cy.js
@@ -0,0 +1,27 @@
+
+describe("Todo App - edit Task", () => {
+    beforeEach(() => {
+      // Assuming your todo app is hosted at http://localhost:3000
+      cy.visit("http://localhost:3000");
+    });
+  
+    it("should edit a task to the todo list", () => {
+      const taskName = "travel to USA at 6PM"; // Replace this with the task you want to add
+  
+      // Find the input field and type the task name
+      cy.get(".form-control").type(taskName);
+  
+      // Find the "Add Task" button and click it
+      cy.get(".btn-success").click();
+  
+     // Find and click on the edit button of the task you want to edit
+      cy.get('.edit-btn').first().click();     
+      // Clear the existing task text and type a new task text
+       cy.get('.edit-form').clear().type('Updated task'); 
+         // Click the save or update button
+       cy.get('.edit-button').click();
+       // Verify that the task has been updated
+      cy.get('.desc-f').first().should('contain', 'Updated task');
+
+    });
+  });
